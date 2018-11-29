d537ee98d1121d6d3a0a0599dc5a3e931a80f9a0
Matthew Cook <mc255v@gmail.com>
2018-11-29 11:59:26 +0900

    added function to create or remove item from list

--- public/app.js
+++ public/app.js
@@ -1,11 +1,69 @@
 $(document).ready(function() {
 	$.getJSON("/api/todos")
 	.then(addTodos);
+
+	$('#todoInput').keypress(function(event) {
+		if(event.which == 13) {
+			createTodo();
+		}
+	});
+
+	$('.list').on('click', 'span', function() {
+		removeTodo($(this).parent());
+	});
 });
 
 function addTodos(todos) {
 	todos.forEach(function(todo) {
-		var newTodo = $('<li class="task">' + todo.name + '</li>');
-		$('.list').append(newTodo);
+		addTodo(todo);
 	});
 }
+
+function addTodo(todo) {
+	var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
+	newTodo.data('id', todo._id);
+	if(newTodo.completed){
+		newTodo.addClass("done");
+	}
+	$('.list').append(newTodo);
+}
+
+function createTodo() {
+	var userInput = $('#todoInput').val();
+	$.post('/api/todos', {name: userInput})
+	.then(function(newTodo) {
+		$('#todoInput').val('');
+		addTodo(newTodo);
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
+}
+
+function removeTodo(todo) {
+	var clickedID = todo.data('id');
+	var deleteUrl = '/api/todos/' + clickedID;
+	$.ajax({
+		method: 'DELETE',
+		url: deleteUrl
+	})
+	.then(function(data) {
+		todo.remove();
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
